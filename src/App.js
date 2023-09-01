2b8ddb1ed5abfb2b9508ade67419a7d3a0e4ee10
IG <iikim2511@gmail.com>
2023-08-31 23:17:24 -0400

    home

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
-
+import  "./App.css";
 
 import Home from './pages/Home';
 import Products from './pages/Products';
@@ -17,29 +17,33 @@ import Product from './pages/Product';
 function App() {
   return (
     <Router>
-      <div className="App">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/products">products</Link>
-            </li>
-            <li>
-              <Link to="/cart">cart</Link>
-            </li>
-          </ul>
-        </nav>  
-  
+      <div className="world">
+          <div className="world-left">
+            <nav className='nav'>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/products">products</Link>
+                </li>
+                <li>
+                  <Link to="/cart">cart</Link>
+                </li>
+              </ul>
+            </nav>  
+          </div>
+      
 
-        <Routes>
-          <Route path="/" element={<Home />} />
+            <div className="world-right">
+              <Routes>
+              <Route path="/" element={<Home />} />
 
-          <Route path="/products" element={<Products/>} />
-          <Route path="/product/:id" element={<product/>} />
-          <Route path="/cart" element={<Cart/>} />
-        </Routes>
+              <Route path="/products" element={<Products/>} />
+              <Route path="/product/:id" element={<product/>} />
+              <Route path="/cart" element={<Cart/>} />
+            </Routes>    
+          </div>
       </div>
     </Router>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+    
+      <h1>Simple ecommerce website with fake shop API</h1>
+      <br />
+    
+
+      <h2>skills: react, redux, fetch api, router</h2>
+      <br />
+    
+      <h1>Let's click Procucts tab on navigation</h1>
+      <br />
+
+      <h1>(You can see my CSS skills from my the other porjects on my portfolio)</h1>
+      
+    </div>
   )
 }
 
