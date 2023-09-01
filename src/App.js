f35f02bc31d66857545a03cfd411d58f84ea2e01
IG <iikim2511@gmail.com>
2023-08-31 22:56:28 -0400

    route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 
 // router install
 // redux install
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+
+
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Cart from './pages/Cart';
+import Product from './pages/Product';
+
+
+
 
 function App() {
   return (
-    <div>
-    
-      hi 
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/products">products</Link>
+            </li>
+            <li>
+              <Link to="/cart">cart</Link>
+            </li>
+          </ul>
+        </nav>  
+  
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/products" element={<Products/>} />
+          <Route path="/product/:id" element={<product/>} />
+          <Route path="/cart" element={<Cart/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/trash,Test/index.js
+++
@@ -1,8 +0,0 @@
-/* image, data, video */
-
-import data from '../constants/data';
-import images from './images';
-
-import meal from '../constants-assets/meal.mp4';
-
-export { data,images, meal };
